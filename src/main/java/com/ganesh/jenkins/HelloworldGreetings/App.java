8e157a2bf5a4cfafaf6577f7300ae6c9bbcf1011
Ganesh Bandreddy <ganeshbandreddy@gmail.com>
2018-06-17 00:03:57 +0530

    Committing new files

---
+++ src/main/java/com/ganesh/jenkins/HelloworldGreetings/App.java
@@ -0,0 +1,13 @@
+package com.ganesh.jenkins.HelloworldGreetings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Greetings" );
+    }
+}
